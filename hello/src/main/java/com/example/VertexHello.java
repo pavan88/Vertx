5738d823305042515ef0c4382eb324ddf853f9c8
Kulkarni <WM87@global.Tesco.org>
2016-05-24 17:23:16 +0530

    First Commit

---
+++ hello/src/main/java/com/example/VertexHello.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class VertexHello extends AbstractVerticle {
+
+	@Override
+	public void start(Future<Void> fut) {
+		vertx.createHttpServer().requestHandler(r -> {
+			r.response().end("<h1>Hello from my first " + "Vert.x 3 application</h1>");
+		}).listen(8080, result -> {
+			if (result.succeeded()) {
+				fut.complete();
+			} else {
+				fut.fail(result.cause());
+			}
+		});
+	}
+
+}
